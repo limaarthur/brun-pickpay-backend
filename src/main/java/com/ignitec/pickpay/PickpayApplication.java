84a429fff7357e5afe7a85417df47c06511eb941
limaarthur <artglima.dev@gmail.com>
2025-01-19 09:52:35 -0300

    first commit

---
+++ src/main/java/com/ignitec/pickpay/PickpayApplication.java
@@ -0,0 +1,13 @@
+package com.ignitec.pickpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignitec/pickpay/PickpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignitec.pickpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
